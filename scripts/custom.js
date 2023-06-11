16f9d2a3bf53c3ab41058bf2a7715bcb2167e5d6
lcuevas <luiselcue@gmail.com>
2023-06-11 03:21:48 +0200

    feat(header): se añade funcionalidad de background del header

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,1 +1,13 @@
-console.log('PRUEBA');
+const backgroundWrapper = document.getElementById('header-featured-image-wrapper');
+backgroundWrapper.style.opacity = 1;
+const scrollLimit = 300;
+window.onscroll = function() {
+  let backgroundOpacit;
+  const currentScroll = window.scrollY;
+  if (currentScroll <= scrollLimit) {
+    backgroundOpacity = parseFloat(1 - currentScroll/scrollLimit).toFixed(2);
+  } else {
+    backgroundOpacity = 0;
+  }
+  backgroundWrapper.style.opacity = backgroundOpacity;
+}
